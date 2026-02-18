32ac82bbb7a8cfa9e8e54452afe3b21b7f863675
Chyavan Shenoy <chyavandshenoy2318@gmail.com>
2026-02-18 21:59:26 +0530

    Basic DB setup | Need to refine DB creation

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,4 +1,14 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            external: ['better-sqlite3']
+        }
+    },
+    resolve: {
+        browserField: false,
+        mainFields: ['module', 'jsnext:main', 'jsnext'],
+    }
+});
