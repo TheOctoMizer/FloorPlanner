a8f0c86ead02696809309cd1419d02b10bb94ec7
Chyavan Shenoy <chyavandshenoy2318@gmail.com>
2026-02-21 09:02:55 +0530

    Added typography to make the rendering better

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,5 +48,8 @@ export default {
             },
         },
     },
-    plugins: [require("tailwindcss-animate")],
+    plugins: [
+        require("tailwindcss-animate"),
+        require("@tailwindcss/typography"),
+    ],
 }
